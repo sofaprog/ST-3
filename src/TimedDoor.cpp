17021690bd91cfd99c9c1f0887c3c2a792e73a84
sofaprog <sofia_makoveeva@mail.ru>
2026-04-06 19:14:30 +0300

    Fix code style issues (cpplint)

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 Sofia Makoveeva
 #include "TimedDoor.h"
 #include <thread>
 #include <chrono>
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,3 +1,6 @@
+// Copyright 2024 Sofia Makoveeva
+// This file contains unit tests for TimedDoor, Timer and DoorTimerAdapter
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <thread>
@@ -8,123 +11,160 @@
 using ::testing::Exactly;
 using ::testing::AtLeast;
 
+// ==================== Тесты для TimedDoor ====================
+
 class TimedDoorTest : public ::testing::Test {
-protected:
-    TimedDoor* door;
-    
-    void SetUp() override {
-        door = new TimedDoor(2);
-    }
-    void TearDown() override {
-        delete door;
-    }
+ protected:  // Отступ 1 пробел перед protected
+  TimedDoor* door;
+
+  void SetUp() override {
+    door = new TimedDoor(2);
+  }
+
+  void TearDown() override {
+    delete door;
+  }
 };
 
+// Тест 1: Проверка начального состояния (дверь закрыта)
 TEST_F(TimedDoorTest, DoorInitiallyClosed) {
-    EXPECT_FALSE(door->isDoorOpened());
-}
-TEST_F(TimedDoorTest, TimeoutValueStored) {
-    EXPECT_EQ(2, door->getTimeOut());
-}
-TEST_F(TimedDoorTest, UnlockOpensDoor) {
-    door->unlock();
-    EXPECT_TRUE(door->isDoorOpened());
+  EXPECT_FALSE(door->isDoorOpened());
 }
 
+// Тест 2: Проверка сохранения таймаута
+TEST_F(TimedDoorTest, TimeoutValueStored) {
+  EXPECT_EQ(2, door->getTimeOut());
+}
+
+// Тест 3: Проверка открытия двери
+TEST_F(TimedDoorTest, UnlockOpensDoor) {
+  door->unlock();
+  EXPECT_TRUE(door->isDoorOpened());
+}
+
+// Тест 4: Проверка закрытия двери
 TEST_F(TimedDoorTest, LockClosesDoor) {
-    door->unlock();
-    door->lock();
-    EXPECT_FALSE(door->isDoorOpened());
+  door->unlock();
+  door->lock();
+  EXPECT_FALSE(door->isDoorOpened());
 }
+
+// Тест 5: Проверка исключения
 TEST_F(TimedDoorTest, ThrowStateThrowsRuntimeError) {
-    EXPECT_THROW(door->throwState(), std::runtime_error);
+  EXPECT_THROW(door->throwState(), std::runtime_error);
 }
+
+// ==================== Тесты для DoorTimerAdapter ====================
 
 class DoorTimerAdapterTest : public ::testing::Test {
-protected:
-    TimedDoor* door;
-    DoorTimerAdapter* adapter;
-    void SetUp() override {
-        door = new TimedDoor(1);
-        adapter = new DoorTimerAdapter(*door);
-    }
-    void TearDown() override {
-        delete door;
-        delete adapter;
-    }
+ protected:  // Отступ 1 пробел перед protected
+  TimedDoor* door;
+  DoorTimerAdapter* adapter;
+
+  void SetUp() override {
+    door = new TimedDoor(1);
+    adapter = new DoorTimerAdapter(*door);
+  }
+
+  void TearDown() override {
+    delete door;
+    delete adapter;
+  }
 };
+
+// Тест 6: Адаптер выбрасывает исключение при открытой двери
 TEST_F(DoorTimerAdapterTest, TimeoutThrowsIfDoorOpen) {
-    door->unlock();
-    EXPECT_THROW(adapter->Timeout(), std::runtime_error);
+  door->unlock();
+  EXPECT_THROW(adapter->Timeout(), std::runtime_error);
 }
+
+// Тест 7: Адаптер не выбрасывает исключение при закрытой двери
 TEST_F(DoorTimerAdapterTest, TimeoutNoThrowIfDoorClosed) {
-    door->lock();
-    EXPECT_NO_THROW(adapter->Timeout());
+  door->lock();
+  EXPECT_NO_THROW(adapter->Timeout());
 }
+
+// ==================== Тесты для Timer с моками ====================
 
 class MockTimerClient : public TimerClient {
-public:
-    MOCK_METHOD(void, Timeout, (), (override));
+ public:  // Отступ 1 пробел перед public
+  MOCK_METHOD(void, Timeout, (), (override));
 };
+
+// Тест 8: Проверка вызова Timeout у клиента
 TEST(TimerTest, TimerCallsTimeout) {
-    MockTimerClient client;
-    EXPECT_CALL(client, Timeout()).Times(Exactly(1));
-    Timer timer;
-    timer.tregister(0, &client);
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  MockTimerClient client;
+  EXPECT_CALL(client, Timeout()).Times(Exactly(1));
+
+  Timer timer;
+  timer.tregister(0, &client);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
 }
 
+// Тест 9: Проверка что таймер не вызывает после остановки
 TEST(TimerTest, TimerDoesNotCallAfterStop) {
-    MockTimerClient client;
-    EXPECT_CALL(client, Timeout()).Times(Exactly(0));
-    Timer timer;
-    timer.tregister(1, &client);
-    timer.stopWorker();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1100));
+  MockTimerClient client;
+  EXPECT_CALL(client, Timeout()).Times(Exactly(0));
+
+  Timer timer;
+  timer.tregister(1, &client);
+  timer.stopWorker();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1100));
 }
+
+// ==================== Интеграционные тесты ====================
 
 class TestableDoorTimerAdapter : public DoorTimerAdapter {
-private:
-    std::exception_ptr* storedException;
-public:
-    TestableDoorTimerAdapter(TimedDoor& door, std::exception_ptr* ex)
-        : DoorTimerAdapter(door), storedException(ex) {}
-    void Timeout() override {
-        try {
-            DoorTimerAdapter::Timeout();
-        } catch (...) {
-            *storedException = std::current_exception();
-        }
+ private:  // Отступ 1 пробел перед private
+  std::exception_ptr* storedException;
+
+ public:  // Отступ 1 пробел перед public
+  explicit TestableDoorTimerAdapter(TimedDoor& door,
+                                     std::exception_ptr* ex)
+      : DoorTimerAdapter(door), storedException(ex) {}
+
+  void Timeout() override {
+    try {
+      DoorTimerAdapter::Timeout();
+    } catch (...) {
+      *storedException = std::current_exception();
     }
+  }
 };
 
 class IntegrationTest : public ::testing::Test {
-protected:
-    TimedDoor* door;
-    TestableDoorTimerAdapter* testAdapter;
-    std::exception_ptr exception;
-    void SetUp() override {
-        door = new TimedDoor(1);
-        testAdapter = new TestableDoorTimerAdapter(*door, &exception);
-        door->changeAdapter(testAdapter);
-    }
-    void TearDown() override {
-        delete door;
-    }
+ protected:  // Отступ 1 пробел перед protected
+  TimedDoor* door;
+  TestableDoorTimerAdapter* testAdapter;
+  std::exception_ptr exception;
+
+  void SetUp() override {
+    door = new TimedDoor(1);
+    testAdapter = new TestableDoorTimerAdapter(*door, &exception);
+    door->changeAdapter(testAdapter);
+  }
+
+  void TearDown() override {
+    delete door;
+  }
 };
 
-// Тест 10: Интеграция - исключение выбрасывается если дверь открыта дольше таймаута
+// Тест 10: Интеграция - исключение выбрасывается
+// если дверь открыта дольше таймаута
 TEST_F(IntegrationTest, ThrowsWhenDoorOpenAfterTimeout) {
-    door->unlock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1050));
-    ASSERT_NE(exception, nullptr);
+  door->unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(1050));
+  ASSERT_NE(exception, nullptr);
 }
 
-// Тест 11: Интеграция - исключение НЕ выбрасывается если дверь закрыта вовремя
+// Тест 11: Интеграция - исключение НЕ выбрасывается
+// если дверь закрыта вовремя
 TEST_F(IntegrationTest, NoThrowWhenDoorClosedBeforeTimeout) {
-    door->unlock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    door->lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(600));
-    ASSERT_EQ(exception, nullptr);
+  door->unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  door->lock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(600));
+  ASSERT_EQ(exception, nullptr);
 }
